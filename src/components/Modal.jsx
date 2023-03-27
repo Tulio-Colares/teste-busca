e64bfcd11d87e5d06119c88287e4cbdcfb2e23dd
Tulio <tulio.colares@portfoliotech.com.br>
2023-03-27 11:46:20 -0300

    feat: conclui modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,40 @@
-import React, {useContext} from 'react';
-import {PesquisaContext} from '../context/pesquisaContext';
+import React, {useContext} from "react";
+import { PesquisaContext } from "../context/pesquisaContext";
+import "./Modal.css";
 
-const Modal = () => {
-  const { setOpenModal, pesquisados, setPesquisados} = useContext(PesquisaContext)
+function Modal() {
+  const { setOpenModal, setPesquisados} = useContext(PesquisaContext)
 
   const clearHistory = () => {
-    setPesquisados([])
+    setPesquisados([]);
     setOpenModal(false)
   }
 
   return (
-    <div className='modalBackground'>
-      <div className='modalContainer'>
-        <button onClick={() => setOpenModal(false)}>X</button>
-        <div className='title'>
-        <h1>Tem certeza de que quer apagar todos os dados do histórico?</h1>
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => setOpenModal(false)}
+          >
+            X
+          </button>
         </div>
-        <div className='body'></div>
-        <div className='footer'>
-          <button onClick={() => setOpenModal(false)}>Cancelar</button>
-          <button onClick={clearHistory}>Apagar!</button>
+        <div className="title">
+          <h1>Tem certeza de que deseja apagar o historico?</h1>
+        </div>
+        <div className="footer">
+          <button
+            onClick={() => setOpenModal(false)}
+            id="cancelBtn"
+          >
+            Cancelar
+          </button>
+          <button onClick={clearHistory}>Sim, apagar!</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
--- src/components/Vitrine.jsx
+++ src/components/Vitrine.jsx
@@ -1,16 +1,28 @@
 import React, {useContext} from 'react';
 import { PesquisaContext } from '../context/pesquisaContext'
-import Modal2 from './Modal2'
+import Modal from './Modal'
+import "./Modal.css";
 
 const Vitrine = () => {
   const {pesquisados, openModal, setOpenModal} = useContext(PesquisaContext)
+
+  const styleBtn = {
+    width: '250px',
+    height: '35px',
+    margin: '10px',
+    border: 'none',
+    backgroundColor: '#87CEEB',
+    color: 'white',
+    borderRadius: '8px',
+    fontSize: '20px',
+    cursor: 'pointer',
+  }
   
   return (
     <div>
       <h2>Pesquisados Recentemente</h2>
-      <button onClick={() => console.log(pesquisados)}>Mostrar console</button>
-      <button onClick={() => setOpenModal(true)}>Apagar histórico</button>
-        {openModal && <Modal2/>}
+      <button onClick={() => setOpenModal(true)} style={styleBtn} >Apagar histórico</button>
+        {openModal && <Modal/>}
         {pesquisados.map(pesquisado => (
             <p key={pesquisado.id}>{pesquisado.suggestion}</p>
         ))}
